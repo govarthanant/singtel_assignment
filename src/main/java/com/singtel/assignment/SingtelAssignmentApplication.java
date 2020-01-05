8ac1767af238f340e17b8ac7b10caa5b3f58760b
govarthanant <24688202+govarthanant@users.noreply.github.com>
2020-01-05 09:33:16 +0800

    Part E count the animals with its behaviours
    Part E count the animals with its behaviours

--- src/main/java/com/singtel/assignment/SingtelAssignmentApplication.java
+++ src/main/java/com/singtel/assignment/SingtelAssignmentApplication.java
@@ -1,37 +1,65 @@
 package com.singtel.assignment;
 
 import com.singtel.assignment.animals.Animals;
+import com.singtel.assignment.animals.aquatic.ClownFish;
+import com.singtel.assignment.animals.aquatic.Dolphins;
 import com.singtel.assignment.animals.aquatic.Fish;
 import com.singtel.assignment.animals.aquatic.Shark;
-import com.singtel.assignment.animals.birds.Duck;
-import com.singtel.assignment.animals.birds.Parrots;
-import com.singtel.assignment.animals.birds.ParrotsType;
-import com.singtel.assignment.animals.birds.Rooster;
+import com.singtel.assignment.animals.birds.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
 @Slf4j
 public class SingtelAssignmentApplication {
 
 	public static void main(String[] args) {
-		//SpringApplication.run(SingtelAssignmentApplication.class, args);
+		Animals[] animals = new Animals[]{
+				new Duck(),
+				new Chicken(),
+				new Rooster(),
+				new Parrots(ParrotsType.PARROT_WITH_CATS),
+				new Parrots(ParrotsType.PARROT_WITH_DOGS),
+				new Parrots(ParrotsType.PARROT_WITH_ROOSTER),
+				new Fish(),
+				new Shark(),
+				new ClownFish(),
+				new Dolphins()
+		};
 
-		Animals duck = new Duck();
-		duck.performFly();
+		Map<String, Integer> behaviourMap = getBehavioursCount(animals);
+		log.info(" :::::::::: Count Result :::::::::::::");
+		behaviourMap.forEach((behaviour, count) ->
+				log.info(behaviour + " " + count));
 
-		Animals rooster = new Rooster();
-		rooster.performSing();
-		rooster.performWalk();
+	}
 
-		Animals parrot = new Parrots(ParrotsType.PARROT_WITH_CATS);
-		parrot.performSing();
+	private static Map<String, Integer> getBehavioursCount(Animals[] animals) {
 
-		Animals fish = new Fish();
-		fish.performSwim();
+		int flyCount = 0;
+		int walkCount = 0;
+		int singCount = 0;
+		int swimCount = 0;
+		for (Animals animal : animals){
+			if (animal.performFly())
+				flyCount = flyCount + 1;
+			if (animal.performWalk())
+				walkCount = walkCount + 1;
+			if (animal.performSing())
+				singCount = singCount + 1;
+			if (animal.performSwim())
+				swimCount = swimCount + 1;
+		}
 
-		Animals shark = new Shark();
-		log.debug("sharks color :: {} ", ((Shark) shark).colour);
+		Map<String, Integer> behaviourMap = new HashMap<>();
+		behaviourMap.put("fly", flyCount);
+		behaviourMap.put("walk", walkCount);
+		behaviourMap.put("sing", singCount);
+		behaviourMap.put("swim", swimCount);
+		return behaviourMap;
 	}
 
 }
